924aff2b3e42da4857104d27e4d38bce904ad7f6
bspammer <charliecollard0@gmail.com>
2016-07-12 16:27:02 +0100

    fixed typo

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 ln -sf "$HOME"/.dotfiles/.bashrc        "$HOME"
 ln -sf "$HOME"/.dotfiles/.compton.conf	"$HOME"
 ln -sf "$HOME"/.dotfiles/.fonts         "$HOME"
-ln -sf "$HOME"/.dotfiles/.vimrc         "$HME"
+ln -sf "$HOME"/.dotfiles/.vimrc         "$HOME"
 ln -sf "$HOME"/.dotfiles/.i3            "$HOME"
