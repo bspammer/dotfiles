3a53abda0cce2e4f58570d8bd8f8673024f13620
Bspammer <charliecollard0@gmail.com>
2016-10-06 14:14:39 +0100

    Minor formatting changes in install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ln -sf "$HOME"/.dotfiles/.bashrc        "$HOME"
-ln -sf "$HOME"/.dotfiles/.compton.conf	"$HOME"
+ln -sf "$HOME"/.dotfiles/.compton.conf  "$HOME"
 ln -sf "$HOME"/.dotfiles/.fonts         "$HOME"
 ln -sf "$HOME"/.dotfiles/.vimrc         "$HOME"
 ln -sf "$HOME"/.dotfiles/.i3            "$HOME"
@@ -9,5 +9,5 @@ ln -sf "$HOME"/.dotfiles/.i3            "$HOME"
 
 mkdir -p "$HOME"/.vim/bundle
 mkdir -p "$HOME"/.vim/undofiles
-cd "$HOME"/.vim/bundle
+cd "$HOME/.vim/bundle"
 git clone https://github.com/VundleVim/Vundle.vim.git
