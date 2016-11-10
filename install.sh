258e8744ce444af1f61930d02e128e56953cd388
Bspammer <charliecollard0@gmail.com>
2016-11-10 15:33:16 +0000

    Added conditionals to Vundle cloning

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echoerr() { echo "$@" 1>&2; }
+
+
 ln -s "$HOME/.dotfiles/.bashrc"        "$HOME"
 ln -s "$HOME/.dotfiles/.compton.conf"  "$HOME"
 ln -s "$HOME/.dotfiles/.fonts"         "$HOME"
@@ -10,4 +13,14 @@ ln -s "$HOME/.dotfiles/.i3"            "$HOME"
 mkdir -p "$HOME"/.vim/bundle
 mkdir -p "$HOME"/.vim/undofiles
 cd "$HOME/.vim/bundle"
-git clone https://github.com/VundleVim/Vundle.vim.git
+if [ ! -d Vundle.vim ]
+then
+    echoerr "Attempting to clone Vundle plugin..."
+    if git clone https://github.com/VundleVim/Vundle.vim.git; then
+        echoerr "Successfully cloned Vundle plugin"
+    else
+        echoerr "Could not clone Vundle plugin - internet down?"
+    fi
+else
+    echoerr "Vundle plugin already exists, skipping..."
+fi
