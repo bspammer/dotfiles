8eb19d5626d7e991cc04d5ea5202f15aa50413c3
Charlie Collard <charles.collard2@philips.com>
2016-11-24 15:19:42 +0000

    Added ssh config file

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ ln -s "$HOME/.dotfiles/.fonts"         "$HOME"
 ln -s "$HOME/.dotfiles/.vimrc"         "$HOME"
 ln -s "$HOME/.dotfiles/.i3"            "$HOME"
 ln -s "$HOME/.dotfiles/.tmux.conf"     "$HOME"
+ln -s "$HOME/.dotfiles/.ssh/config"    "$HOME/.ssh"
 
 
 mkdir -p "$HOME"/.vim/bundle
