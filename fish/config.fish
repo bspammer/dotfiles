5dea0d2b6d7e4c0a0187a459f8c3097d918e2eba
Charlie Collard <charliecollard0@gmail.com>
2019-06-18 11:33:18 +0100

    Remove machine-specific path

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,3 @@ set -x XDG_DATA_HOME 	$HOME/.local/share
 set -x XDG_CONFIG_HOME 	$HOME/.config
 set -x XDG_CACHE_HOME 	$HOME/.cache
 set -x VIMINIT 		":source $XDG_CONFIG_HOME/vim/vimrc"
-set -x PATH 		/Users/charliecollardlt1/bin $PATH
-set -x PATH 		/Users/charliecollardlt1/go/bin $PATH
