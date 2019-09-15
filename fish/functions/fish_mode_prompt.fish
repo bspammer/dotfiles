fd62bad45fc21ab5a9baae7b58f77164b302ccf7
Charlie Collard <charliecollard0@gmail.com>
2019-09-15 20:56:37 +0100

    Enable vim mode for fish

---
+++ fish/functions/fish_mode_prompt.fish
@@ -0,0 +1,21 @@
+function fish_mode_prompt --description 'Displays the current mode'
+    # Do nothing if not in vi mode
+    if test "$fish_key_bindings" = "fish_vi_key_bindings"
+        switch $fish_bind_mode
+            case default
+                set_color --bold red
+                echo █
+            case insert
+                set_color --bold green
+                echo █
+            case replace-one
+                set_color --bold green
+                echo 🅡
+            case visual
+                set_color --bold brmagenta
+                echo 🅥
+        end
+        set_color normal
+        printf " "
+    end
+end
