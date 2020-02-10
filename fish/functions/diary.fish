972f948e244db23ca031cad2f565e77bedc59184
Charlie Collard <charliecollard0@gmail.com>
2020-02-10 15:08:14 +0000

    Add diary function

---
+++ fish/functions/diary.fish
@@ -0,0 +1,8 @@
+function diary --description "Opens my diary with a given day offset"
+    if set -q argv[1]
+        set offset $argv[1]
+    else
+        set offset 0
+    end
+    vim (python3 -c "import datetime; import os; print((datetime.datetime.now() + datetime.timedelta(days=$offset)).strftime(os.environ['HOME'] + '/Dropbox/docs/diary/%Y-%m-%d-%a.txt'))")
+end
