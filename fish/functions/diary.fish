2545aa8c704fe0f4f8f77eead5a7437b5aeb7f31
Charlie Collard <charliecollard0@gmail.com>
2020-10-18 15:11:03 +0100

    Update diary path

--- fish/functions/diary.fish
+++ fish/functions/diary.fish
@@ -4,5 +4,5 @@ function diary --description "Opens my diary with a given day offset"
     else
         set offset 0
     end
-    vim (python3 -c "import datetime; import os; print((datetime.datetime.now() + datetime.timedelta(days=$offset)).strftime(os.environ['HOME'] + '/Dropbox/docs/diary/%Y-%m-%d-%a.txt'))")
+    vim (python3 -c "import datetime; import os; print((datetime.datetime.now() + datetime.timedelta(days=$offset)).strftime(os.environ['HOME'] + '/Dropbox/docs/diaries/%Y/%Y-%m-%d-%a.txt'))")
 end
