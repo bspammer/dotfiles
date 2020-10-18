f053c71239a31469934c5ec2f0a93bf3dd873eb6
Charlie Collard <charliecollard0@gmail.com>
2020-10-18 15:18:35 +0100

    fish: Set EDITOR variable

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
-set -x XDG_DATA_HOME 	$HOME/.local/share
-set -x XDG_CONFIG_HOME 	$HOME/.config
-set -x XDG_CACHE_HOME 	$HOME/.cache
-set -x VIMINIT 		":source $XDG_CONFIG_HOME/vim/vimrc"
+set -x EDITOR          vim
+set -x XDG_DATA_HOME   $HOME/.local/share
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_CACHE_HOME  $HOME/.cache
+set -x VIMINIT         ":source $XDG_CONFIG_HOME/vim/vimrc"
 fish_vi_key_bindings
