699612fc727c2eea67d06b307c164bbc89deea20
Charlie Collard <charliecollard0@gmail.com>
2020-11-10 14:41:03 +0000

    Show current git branch in fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,7 +3,9 @@ function fish_prompt
     and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
     test "$USER" = 'root'
     and echo (set_color red)"#"
+    set GIT_BRANCH " "(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+    test GIT_BRANCH = " ()"; and set GIT_BRANCH ""
 
     # Main
-    echo -n (set_color cyan)(prompt_pwd) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+    echo -n (set_color cyan)(prompt_pwd)(set_color purple)"$GIT_BRANCH" (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
 end
