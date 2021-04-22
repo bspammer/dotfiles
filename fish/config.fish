6834f98340e9ec7ebae21207a5b74705f3dafada
Charlie Collard <charliecollard0@gmail.com>
2021-04-22 12:56:49 +0100

    Add CDPATH environment variable

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,6 @@ set -x EDITOR          vim
 set -x XDG_DATA_HOME   $HOME/.local/share
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME  $HOME/.cache
+set -x CDPATH          . $HOME/workspace
 set -x VIMINIT         ":source $XDG_CONFIG_HOME/vim/vimrc"
 fish_vi_key_bindings
